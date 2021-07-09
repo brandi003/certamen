481ed27b922ad3a3e74ee71dd1bb303e7b4f0584
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-07-09 15:41:36 -0400

    a

---
+++ code/src/main.cc
@@ -0,0 +1,41 @@
+#include <boost/mpi.hpp>
+#include <iostream>
+#include <random>
+#include <thread> 
+#include <chrono>
+#include <cmath>
+#include <cstring>
+
+
+namespace mpi = boost::mpi;
+
+int main(int argc, char *argv[])
+{
+	mpi::environment env{argc, argv};
+	mpi::communicator world;
+	
+	int rankID     = world.rank();
+	int procInComm = world.size();
+	
+	double step;
+	uint32_t num_steps = 100000;
+	
+	
+	double piValue, sum = 0.0;
+	step = 1.0/(double)num_steps;
+
+	uint32_t inicio, fin;
+	inicio = 1;
+	fin    = num_steps;
+	for (size_t i = 1; i <= fin; i++){
+		double x;
+		
+		x = (i-0.5)*step;
+		sum = sum + 4.0/(1.0+x*x);
+	}
+	piValue = step * sum;
+	
+	std::cout << "pi=" << piValue << std::endl;
+	return(EXIT_SUCCESS);
+}
+
